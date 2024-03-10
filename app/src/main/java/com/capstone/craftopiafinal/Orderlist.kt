3c542c3a44f19f5b792ba256c13c1984741b3b3b
JTLshesh <limpagjethro@gmail.com>
2024-03-09 16:25:40 -0800

    unfinished yet

---
+++ app/src/main/java/com/capstone/craftopiafinal/Orderlist.kt
@@ -0,0 +1,11 @@
+package com.capstone.craftopiafinal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Orderlist : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_orderlist)
+    }
+}
